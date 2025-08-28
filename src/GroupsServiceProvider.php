49aff029293a4aa4ff6e2cadf24cc678446111c9
Custură Laurențiu <127675571+custura@users.noreply.github.com>
2025-08-28 12:34:03 +0200

    Create GroupsServiceProvider.php

---
+++ src/GroupsServiceProvider.php
@@ -0,0 +1,64 @@
+<?php namespace Zaimea\GroupsSDK;
+
+use Zaimea\GroupsSDKPHP;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * AWS SDK for PHP service provider for Laravel applications
+ */
+class AwsServiceProvider extends ServiceProvider
+{
+    const VERSION = '1.0';
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Bootstrap the configuration
+     *
+     * @return void
+     */
+    public function boot()
+    {
+          $this->publishes(
+              [__DIR__.'/../config/groups.php' => config_path('groups.php')],
+              'groups'
+          );
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/groups.php',
+            'groups'
+        );
+
+        $this->app->singleton('groups', function ($app) {
+            $config = $app->make('config')->get('groups');
+
+            return new GroupsSDKPHP($config);
+        });
+
+        $this->app->alias('groups', 'Zaimea\GroupsSDKPHP');
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['groups', 'Zaimea\GroupsSDKPHP'];
+    }
+
+}
