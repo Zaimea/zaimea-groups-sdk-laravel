289f3b19f34fb3220ad09acc2b5f082e84227c7a
Custură Laurențiu <127675571+custura@users.noreply.github.com>
2025-08-28 12:36:30 +0200

    Update GroupsFacade.php

--- src/GroupsFacade.php
+++ src/GroupsFacade.php
@@ -1,1 +1,24 @@
+<?php namespace Zaimea\GroupsSDK;
 
+use Zaimea\GroupsClientInterface;
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * Facade for the Zaimea Groups service
+ *
+ * @method static GroupsClientInterface createClient($name, array $args = []) Get a client from the service builder.
+ */
+class GroupsFacade extends Facade
+{
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'groups';
+    }
+
+}
